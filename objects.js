6256a3bbdd1cda6f4ab0707e53a77fc225103448
Milo MacPhail <milomacphail@gmail.com>
2018-08-19 15:56:37 +0000

    Lesson-8

--- objects.js
+++ objects.js
@@ -19,11 +19,26 @@ class User  {
   }
 }
 
+class Admin extends User {
+  
+  deleteUser(user){
+    users = users.filter(u => {
+      return u.email != user.email
+    })
+  }  
+  
+  
+}
+
+
+
+
 var userOne = new User ("ryu@ninjas.com", "Ryu");
 var userTwo = new User ("yoshi@mariokorp.com", "Yoshi");
+var admin = new Admin ("shaun@ninjas.com", "Shaun")
+var users =  [userOne, userTwo, admin];
 
-console.log(userOne);
-console.log(userTwo);
 
-userOne.login().updateScore().updateScore().logout();
-userTwo.logout();
+admin.deleteUser(userOne);
+console.log(users);
+
